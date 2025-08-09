57eea37d2c4fa6c9dc5a88c9cd130c5263d6e03e
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2025-08-09 20:23:03 +0000

    commit basic ui

--- app/src/main/java/com/example/myjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/myjetpackcompose/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.myjetpackcompose.ui.theme.MyJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,29 +23,74 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                BasicUi()
+
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun BasicUi() {
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+//            .background(MaterialTheme.colorScheme.primaryContainer),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+
+
+        )
+        {
+
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Row 1"
+                )
+            }
+
+            Button(
+                onClick = {}
+            ) {
+                Text(
+                    text = "Row 2"
+                )
+            }
+
+        }
+
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyJetpackComposeTheme {
-        Greeting("Android")
-    }
-}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyJetpackComposeTheme {
+//        Greeting("Android")
+//    }
+//}
